e41695192b7ba9c22ffa7d5005d664b520011faf
ouram <mboivin@student.42.fr>
2020-12-03 18:48:47 +0100

    wip: Parse pipe sequences

--- src/parsing/parser/parse_pipe_sequence.c
+++ src/parsing/parser/parse_pipe_sequence.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 20:04:56 by mboivin           #+#    #+#             */
-/*   Updated: 2020/12/03 17:02:40 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/12/03 18:48:18 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,28 @@
 ** pipe_sequence :                    simple_command
 **               | pipe_sequence PIPE simple_command
 **
+**      NODE_PIPE_SEQ
+**            |
+**     NODE_SIMPLE_CMD
+**            |
+**           ...
+**
 ** returns: true if the node creation succeeded
 **          false otherwise
 */
 
 bool	parse_pipe_sequence(t_ast_node **ast, t_lexer *lexer, size_t *pos)
 {
-	(void)ast;
-	(void)lexer;
-	(void)pos;
+	t_ast_node	*pipe_node;
+
+	pipe_node = malloc_ast_node(NODE_PIPE_SEQ, "|");
+	if (parse_simple_cmd(&pipe_node, lexer, pos) && (*pos < lexer->size))
+	{
+		append_node_right(ast, pipe_node);
+		if (is_expected_type(lexer->tokens[*pos]->type, TOKEN_PIPE))
+			(*pos)++;
+		return (true);
+	}
+	free_ast(&pipe_node);
 	return (false);
 }
