8b6fd43600357f5fe6ada8aae6de8953b3833470
ouram <mboivin@student.42.fr>
2020-12-12 17:32:27 +0100

    fix: Free env if signal handling fails

--- src/subsystems/signals/register_signal_handlers.c
+++ src/subsystems/signals/register_signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 17:51:19 by mboivin           #+#    #+#             */
-/*   Updated: 2020/12/05 18:22:18 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/12/12 17:31:24 by ouram            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <signal.h>
+#include "sh_env.h"
 #include "sh_subsystems.h"
 
 /*
@@ -23,7 +24,13 @@
 void	register_signal_handlers(void)
 {
 	if (signal(SIGINT, handle_fatal_error) == SIG_ERR)
+	{
+		ft_clearenv();
 		exit(EXIT_FAILURE);
+	}
 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+	{
+		ft_clearenv();
 		exit(EXIT_FAILURE);
+	}
 }
