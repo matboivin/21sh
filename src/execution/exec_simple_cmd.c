0572ed8fda779d042ecbba116421075464282e97
ouram <mboivin@student.42.fr>
2020-12-19 21:06:10 +0100

    wip: Don't exec if no command path

--- src/execution/exec_simple_cmd.c
+++ src/execution/exec_simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 18:16:43 by mboivin           #+#    #+#             */
-/*   Updated: 2020/12/19 20:40:17 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/12/19 21:05:23 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ void		exec_simple_cmd(t_shctrl *ft_sh, t_simplecmd *simple_cmd)
 		redirect_stream(simple_cmd->input_fd, STDIN_FILENO);
 	if (simple_cmd->output_fd != NO_FILE)
 		redirect_stream(simple_cmd->output_fd, STDOUT_FILENO);
-	execve(simple_cmd->cmd_path, simple_cmd->cmd_args, g_env);
+	if (simple_cmd->cmd_path)
+		execve(simple_cmd->cmd_path, simple_cmd->cmd_args, g_env);
 	exit_shell(ft_sh);
 }
