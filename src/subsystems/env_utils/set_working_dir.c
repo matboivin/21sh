1a2c947256216e727f83bfaf5db942c717902927
ouram <mboivin@student.42.fr>
2020-12-28 20:09:27 +0100

    Add backup way to add PWD to env

--- src/subsystems/env_utils/set_working_dir.c
+++ src/subsystems/env_utils/set_working_dir.c
@@ -6,12 +6,13 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/27 17:28:40 by mboivin           #+#    #+#             */
-/*   Updated: 2020/12/27 18:33:22 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/12/28 17:56:44 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <limits.h>
 #include <unistd.h>
+#include "libft_str.h"
 #include "sh_env.h"
 
 /*
@@ -23,9 +24,19 @@
 
 int			set_working_dir(void)
 {
+	int		ret;
+	char	*new_var;
 	char	cwd[PATH_MAX];
 
-	if (getcwd(cwd, PATH_MAX))
+	ret = EXIT_SUCCESS;
+	new_var = NULL;
+	if (!getcwd(cwd, PATH_MAX))
+		return (FAIL_RET);
+	if (ft_findenv("PWD") != FAIL_RET)
 		return (ft_setenv("PWD", cwd, true));
-	return (FAIL_RET);
+	new_var = ft_join_n_str(2, "PWD=", cwd);
+	if (new_var)
+		ret = ft_putenv(new_var);
+	ft_strdel(&new_var);
+	return (ret);
 }
